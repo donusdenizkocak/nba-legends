33646c925c1286b7430e25a95ff109ad54713b71
denizderK <denizderya8668@gmail.com>
2023-04-04 00:55:23 +0300

    edit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import Image from "react-bootstrap/Image";
 const Header =() =>{
     return (
         <Container>
-            <Image src={nbaLogo} width="200px"> </Image>
+            <Image src={nbaLogo} width="200px"></Image>
             <h1 className="my-2 font-monospace display-4 fw-bold">NBA Legends</h1>
         </Container>
     )
